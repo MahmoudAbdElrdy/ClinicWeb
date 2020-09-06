f169e612e30507ec1aec98557720fc4967ba1fa2
Mahmoud Abd-Elradi <mahmoudabd23451@gmail.com>
2020-09-07 00:11:32 +0200

    swaggerDoc

---
+++ ClinicWeb/RemoveVerbsFilter.cs
@@ -0,0 +1,32 @@
+﻿
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http.Description;
+
+
+namespace ClinicWeb
+{
+    public class RemoveVerbsFilter:IDocumentFilter
+    {
+        public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
+        {
+            //var paths = new OpenApiPaths();
+            //foreach (var path in swaggerDoc.Paths)
+            //{
+            //    paths.Add(path.Key.Replace("v{version}", swaggerDoc.Info.Version), path.Value);
+            //}
+            //swaggerDoc.Paths = paths;
+          
+                 swaggerDoc.Components.Schemas.Remove("Day");
+            swaggerDoc.Components.Schemas.Remove("Bills");
+            swaggerDoc.Components.Schemas.Remove("Clinic");
+            swaggerDoc.Components.Schemas.Remove("Doctor");
+        }
+
+
+    }
+}
--- ClinicWeb/Startup.cs
+++ ClinicWeb/Startup.cs
@@ -28,7 +28,8 @@ using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.Swagger;
 using Swashbuckle.AspNetCore.SwaggerUI;
 using Services;
-
+using System.Web.Http.Description;
+using Swashbuckle.Swagger;
 namespace ClinicWeb
 {
   public class Startup
@@ -80,39 +81,42 @@ namespace ClinicWeb
             services.AddSwaggerGen(x =>
             {
                 x.SwaggerDoc("v1", new OpenApiInfo { Title = "project Api", Version = "v1" });
+                x.SchemaFilter<SwaggerExcludeFilter>();
+                x.DocumentFilter<RemoveVerbsFilter>();
+
                 //-----------------------------start jwtSettings swagger ---------------------------------
-        //        var security = new Dictionary<string, IEnumerable<string>>
-        //        {
-        //            {"Bearer",new string[0]}
+                //        var security = new Dictionary<string, IEnumerable<string>>
+                //        {
+                //            {"Bearer",new string[0]}
 
-        //        };
-        //        x.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-        //        {
-        //            Description =
-        //          "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
-        //            Name = "Authorization",
-        //            In = ParameterLocation.Header,
-        //            Type = SecuritySchemeType.ApiKey,
-        //            Scheme = "Bearer"
-        //        });
-        //        x.AddSecurityRequirement(new OpenApiSecurityRequirement()
-        //{
-        //    {
-        //        new OpenApiSecurityScheme
-        //        {
-        //            Reference = new OpenApiReference
-        //            {
-        //                Type = ReferenceType.SecurityScheme,
-        //                Id = "Bearer"
-        //            },
-        //            Scheme = "oauth2",
-        //            Name = "Bearer",
-        //            In = ParameterLocation.Header,
+                //        };
+                //        x.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                //        {
+                //            Description =
+                //          "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+                //            Name = "Authorization",
+                //            In = ParameterLocation.Header,
+                //            Type = SecuritySchemeType.ApiKey,
+                //            Scheme = "Bearer"
+                //        });
+                //        x.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                //{
+                //    {
+                //        new OpenApiSecurityScheme
+                //        {
+                //            Reference = new OpenApiReference
+                //            {
+                //                Type = ReferenceType.SecurityScheme,
+                //                Id = "Bearer"
+                //            },
+                //            Scheme = "oauth2",
+                //            Name = "Bearer",
+                //            In = ParameterLocation.Header,
 
-        //        },
-        //        new List<string>()
-        //    }
-        //});
+                //        },
+                //        new List<string>()
+                //    }
+                //});
                 //-----------------------------end jwtSettings swagger---------------------------------
 
             });// Auto Mapper Configurations
